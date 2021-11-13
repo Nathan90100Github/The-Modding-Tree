93b62050750f1e23b6c7f66f6536c1b5499d375f
Nathan90100Github <94223128+Nathan90100Github@users.noreply.github.com>
2021-11-13 13:52:45 +0100

    stuff

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
 	name: "The Speed Tree",
-	id: "s p e e d",
+	id: "speed is good not speed is not good",
 	author: "Nathan_90100",
 	pointsName: "speed",
 	modFiles: ["layers.js", "tree.js"],
@@ -13,14 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "The beginning of stuff",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Changed some random stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
